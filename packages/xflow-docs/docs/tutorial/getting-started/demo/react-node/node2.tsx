802ba4201f630186359e948a2b2b723f0fa1cf51
小耀 <jinyue.gjy@antfin.com>
2021-11-04 21:16:42 +0800

    docs: perfect quick start demo

--- packages/xflow-docs/docs/tutorial/getting-started/demo/react-node/node2.tsx
+++ packages/xflow-docs/docs/tutorial/getting-started/demo/react-node/node2.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import type { NsGraph } from '@antv/xflow'
+import React from 'react'
 import { useAppContext } from '@antv/xflow'
 import './node2.less'
 
 const Node2: NsGraph.INodeRender = props => {
   const ctx = useAppContext()
-  // console.log(ctx,props)
+
   return (
     <div className="node2-container">
       <div>{'React节点2'}</div>
