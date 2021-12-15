9683c69f04e4ede5cb5d07459c03512819d65e0b
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2021-12-15 14:41:57 +0800

    fix: 修复color-picker render 产生的 warning (#33)
    * fix: 修复color-picker render 产生的 warning
    
    * fix: 调整默认宽度,确保显示全

--- packages/xflow-extension/src/flowchart-editor-panel/control-map-service/components/fields/color.tsx
+++ packages/xflow-extension/src/flowchart-editor-panel/control-map-service/components/fields/color.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef, memo } from 'react'
 import type { IGraphConfig } from '@antv/xflow-core'
 import { useXFlowApp } from '@antv/xflow-core'
-import { render } from 'react-dom'
 import { Button } from 'antd'
 import { SketchPicker } from 'react-color'
 import { PREFIX } from '../constants'
@@ -24,51 +23,34 @@ const ColorPicker: React.FC<IProps> = props => {
 
   const PickContainer = () => {
     return (
-      <div className={`${PREFIX}-popover`}>
-        <SketchPicker
-          style={{
-            width: '100%',
-          }}
-          onChange={color => {
-            colorRef.current = color.hex
-          }}
-        />
-        <div className="foolter">
-          <Button
-            onClick={() => {
-              setShow(false)
+      <div className={`${PREFIX}-pick-color-container`}>
+        <div className={`${PREFIX}-popover`}>
+          <SketchPicker
+            onChange={color => {
+              colorRef.current = color.hex
             }}
-          >
-            取消
-          </Button>
-          <Button
-            type="primary"
-            onClick={() => {
-              onChange?.(colorRef.current)
-              setShow(false)
-            }}
-          >
-            确认
-          </Button>
+          />
+          <div className="foolter">
+            <Button
+              onClick={() => {
+                setShow(false)
+              }}
+            >
+              取消
+            </Button>
+            <Button
+              type="primary"
+              onClick={() => {
+                onChange?.(colorRef.current)
+                setShow(false)
+              }}
+            >
+              确认
+            </Button>
+          </div>
         </div>
       </div>
     )
-  }
-  /**  react-color mouseOver 和上层事件冲突，不得已为之，非 DI 环境应该不存在，后续改进 */
-  const CreatePickColorContainer = (visible: boolean) => {
-    const container = graphConfig.current?.graphContainer || document.body
-    const exist: HTMLDivElement | null = document.querySelector(`#${PREFIX}-pick-color-container`)
-    if (exist) {
-      container.removeChild(exist)
-    }
-    if (!visible) {
-      return
-    }
-    const div = document.createElement('div')
-    div.id = `${PREFIX}-pick-color-container`
-    div.className = `${PREFIX}-pick-color-container`
-    render(<PickContainer />, div)
-    container.appendChild(div)
   }
 
   return (
@@ -88,7 +70,7 @@ const ColorPicker: React.FC<IProps> = props => {
           }}
         />
       </div>
-      {CreatePickColorContainer(show)}
+      {show && <PickContainer />}
     </div>
   )
 }
