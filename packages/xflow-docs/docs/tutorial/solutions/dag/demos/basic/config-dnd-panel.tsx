7014d7f16820f828ef236befc37c97fd8c59e641
lyn-boyu <boyu.zlj@antgroup.com>
2021-12-22 16:55:48 +0800

    chore: replace dnd node panel

--- packages/xflow-docs/docs/tutorial/solutions/dag/demos/basic/config-dnd-panel.tsx
+++ packages/xflow-docs/docs/tutorial/solutions/dag/demos/basic/config-dnd-panel.tsx
@@ -3,11 +3,11 @@ import { uuidv4 } from '@antv/xflow'
 import { XFlowNodeCommands } from '@antv/xflow'
 import { DND_RENDER_ID } from './constant'
 import type { NsNodeCmd } from '@antv/xflow'
-import type { NsNodeTreePanel } from '@antv/xflow'
+import type { NsNodeCollapsePanel } from '@antv/xflow'
 import { Card } from 'antd'
 import React from 'react'
 
-export const onNodeDrop: NsNodeTreePanel.IOnNodeDrop = async (node, commands, modelService) => {
+export const onNodeDrop: NsNodeCollapsePanel.IOnNodeDrop = async (node, commands, modelService) => {
   const args: NsNodeCmd.AddNode.IArgs = {
     nodeConfig: { ...node, id: uuidv4() },
   }
@@ -23,91 +23,92 @@ const NodeDescription = (props: { name: string }) => {
   )
 }
 
-export const treeDataService: NsNodeTreePanel.ITreeDataService = async (meta, modelService) => {
+export const nodeDataService: NsNodeCollapsePanel.INodeDataService = async (meta, modelService) => {
+  console.log(meta, modelService)
   return [
     {
-      id: '1',
-      label: '数据读写',
-      parentId: '',
-      renderKey: DND_RENDER_ID,
+      id: '数据读写',
+      header: '数据读写',
+      children: [
+        {
+          id: '2',
+          label: '算法组件1',
+          parentId: '1',
+          renderKey: DND_RENDER_ID,
+          popoverContent: <NodeDescription name="算法组件1" />,
+        },
+        {
+          id: '3',
+          label: '算法组件2',
+          parentId: '1',
+          renderKey: DND_RENDER_ID,
+          popoverContent: <NodeDescription name="算法组件2" />,
+        },
+        {
+          id: '4',
+          label: '算法组件3',
+          parentId: '1',
+          renderKey: DND_RENDER_ID,
+          popoverContent: <NodeDescription name="算法组件3" />,
+        },
+      ],
     },
     {
-      id: '2',
-      label: '算法组件1',
-      parentId: '1',
-      renderKey: DND_RENDER_ID,
-      popoverContent: <NodeDescription name="算法组件1" />,
+      id: '数据加工',
+      header: '数据加工',
+      children: [
+        {
+          id: '6',
+          label: '算法组件4',
+          parentId: '5',
+          renderKey: DND_RENDER_ID,
+        },
+        {
+          id: '7',
+          label: '算法组件5',
+          parentId: '5',
+          renderKey: DND_RENDER_ID,
+        },
+        {
+          id: '8',
+          label: '算法组件6',
+          parentId: '5',
+          renderKey: DND_RENDER_ID,
+        },
+      ],
     },
     {
-      id: '3',
-      label: '算法组件2',
-      parentId: '1',
-      renderKey: DND_RENDER_ID,
-      popoverContent: <NodeDescription name="算法组件2" />,
-    },
-    {
-      id: '4',
-      label: '算法组件3',
-      parentId: '1',
-      renderKey: DND_RENDER_ID,
-      popoverContent: <NodeDescription name="算法组件3" />,
-    },
-    {
-      id: '5',
-      label: '数据加工',
-      parentId: '',
-      renderKey: DND_RENDER_ID,
-    },
-    {
-      id: '6',
-      label: '算法组件4',
-      parentId: '5',
-      renderKey: DND_RENDER_ID,
-    },
-    {
-      id: '7',
-      label: '算法组件5',
-      parentId: '5',
-      renderKey: DND_RENDER_ID,
-    },
-    {
-      id: '8',
-      label: '算法组件6',
-      parentId: '5',
-      renderKey: DND_RENDER_ID,
-    },
-    {
-      id: '9',
-      label: '模型训练',
-      parentId: '',
-      renderKey: DND_RENDER_ID,
-    },
-    {
-      id: '10',
-      label: '算法组件7',
-      parentId: '9',
-      renderKey: DND_RENDER_ID,
-    },
-    {
-      id: '11',
-      label: '算法组件8',
-      parentId: '9',
-      renderKey: DND_RENDER_ID,
-    },
-    {
-      id: '12',
-      label: '算法组件9',
-      parentId: '9',
-      renderKey: DND_RENDER_ID,
+      id: '模型训练',
+      header: '模型训练',
+      children: [
+        {
+          id: '6',
+          label: '算法组件4',
+          parentId: '5',
+          renderKey: DND_RENDER_ID,
+        },
+        {
+          id: '7',
+          label: '算法组件5',
+          parentId: '5',
+          renderKey: DND_RENDER_ID,
+        },
+        {
+          id: '8',
+          label: '算法组件6',
+          parentId: '5',
+          renderKey: DND_RENDER_ID,
+        },
+      ],
     },
   ]
 }
 
-export const searchService: NsNodeTreePanel.ISearchService = async (
-  treeNodeList: NsNodeTreePanel.ITreeNode[] = [],
+export const searchService: NsNodeCollapsePanel.ISearchService = async (
+  nodes: NsNodeCollapsePanel.IPanelNode[] = [],
   keyword: string,
 ) => {
-  const list = treeNodeList.filter(i => i.isDirectory || i.label.includes(keyword))
-  console.log(list, keyword, treeNodeList)
+  const list = nodes.filter(node => node.label.includes(keyword))
+  console.log(list, keyword, nodes)
   return list
 }
