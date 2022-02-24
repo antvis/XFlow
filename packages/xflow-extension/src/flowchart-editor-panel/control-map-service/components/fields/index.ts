dadb8a586098032cb6d4bb602e0b8c52ade3350c
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2022-02-24 16:28:22 +0800

    feat: 类型&组件导出 (#138)

--- packages/xflow-extension/src/flowchart-editor-panel/control-map-service/components/fields/index.ts
+++ packages/xflow-extension/src/flowchart-editor-panel/control-map-service/components/fields/index.ts
@@ -1,8 +1,18 @@
 import InputFiled from './input'
+import type { IProps } from './color'
 import ColorPicker from './color'
 import InputNumberFiled from './input-number'
 import Size from './size'
 import Position from './position'
 import SelectField from './select'
+
+export const EditorPanels: Record<string, React.FC | React.NamedExoticComponent<IProps>> = {
+  InputFiled,
+  ColorPicker,
+  InputNumberFiled,
+  Size,
+  Position,
+  SelectField,
+}
 
 export { InputFiled, ColorPicker, InputNumberFiled, Size, Position, SelectField }
