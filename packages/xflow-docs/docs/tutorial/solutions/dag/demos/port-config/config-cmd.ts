8e421d89846db3ee49f7709516413cdb93a87806
聂海锋 <niexiaofei@yahoo.com>
2022-04-07 00:08:09 +0800

    Fix: 修复 save hook 拼写

--- packages/xflow-docs/docs/tutorial/solutions/dag/demos/port-config/config-cmd.ts
+++ packages/xflow-docs/docs/tutorial/solutions/dag/demos/port-config/config-cmd.ts
@@ -17,7 +17,7 @@ export const useCmdConfig = createCmdConfig(config => {
           args.graphMetaService = MockApi.queryGraphMeta
         },
       }),
-      hooks.saveGrpahData.registerHook({
+      hooks.saveGraphData.registerHook({
         name: 'save graph data',
         handler: async args => {
           if (!args.saveGraphDataService) {
