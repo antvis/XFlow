5f7f660a6fa1eec372ccdd927462c64fb76b1110
lxfu1 <954055752@qq.com>
2022-08-12 17:58:48 +0800

    feat: 新增 edgeConfig 和 showPortOnNodeSelected

--- packages/xflow-extension/src/flowchart-canvas/utils/events.ts
+++ packages/xflow-extension/src/flowchart-canvas/utils/events.ts
@@ -72,16 +72,76 @@ const getContainer = e => {
   return containter
 }
 
+// 获取选中节点的 ports
+export const getSelectedCellPorts = (eleId: string) => {
+  const selectedPorts = document.getElementsByClassName('x6-node-selected')
+  if (!selectedPorts) {
+    return
+  }
+  let matchPort = selectedPorts[0]
+  Array.from(selectedPorts).forEach((ele: HTMLDivElement) => {
+    if (ele.getAttribute('data-cell-id') === eleId) {
+      matchPort = ele
+    }
+  })
+  return matchPort.getElementsByClassName('x6-port') as any
+}
+
+const setTransformData = (ele: HTMLElement, scale: number) => {
+  const currentTransform = ele.getAttribute('transform')
+  const transforms: (string | number)[] = currentTransform.split(',')
+  transforms[0] = `matrix(${Number((transforms[0] as string).split('(')[1]) * scale}`
+  transforms[3] = Number(transforms[3]) * scale
+  ele.setAttribute('transform', transforms.join(','))
+}
+// 节点 ports 放大2被
+export const setTransform = (elements: HTMLElement[]) => {
+  if (!elements?.length) return
+  const scale = 2
+  elements.forEach(ele => {
+    if (ele.getAttribute('data-scale')) return
+    setTransformData(ele, scale)
+    ele.setAttribute('data-scale', `${scale}`)
+  })
+}
+// 重置节点 scale
+export const resetTransform = () => {
+  const ports = document.getElementsByClassName('x6-port')
+  if (!ports) {
+    return
+  }
+  Array.from(ports).forEach((ele: HTMLElement) => {
+    const eleScale = ele.getAttribute('data-scale')
+    if (eleScale) {
+      setTransformData(ele, 1 / Number(eleScale))
+      ele.removeAttribute('data-scale')
+    }
+  })
+}
+
 /** 设置 ports visible */
-export const changePortsVisible = (visible: boolean, e?: any) => {
+export const changePortsVisible = (
+  visible: boolean,
+  e?: any,
+  showPortsOnNodeSelected?: boolean,
+) => {
+  if (!visible) {
+    resetTransform()
+  }
   const containers = getContainer(e)
+  const graph = getGraphInstance()
+  const selectedCell = graph.getSelectedCells()?.[0]
+  // 节点选中并移入时，port transfrom scale * 2
+  if (selectedCell?.isNode() && showPortsOnNodeSelected) {
+    setTransform(getSelectedCellPorts(selectedCell.id))
+  }
   Array.from(containers).forEach((container: HTMLDivElement) => {
     const ports = container.querySelectorAll('.x6-port-body') as NodeListOf<SVGAElement>
-    const graph = getGraphInstance()
     // 选中中节点时不展示链接桩
-    const isSelectedNode = graph.getSelectedCells()?.[0]?.isNode()
+    const isSelectedNode = selectedCell?.isNode()
     for (let i = 0, len = ports.length; i < len; i = i + 1) {
-      ports[i].style.visibility = !isSelectedNode && visible ? 'visible' : 'hidden'
+      ports[i].style.visibility =
+        (!isSelectedNode || showPortsOnNodeSelected) && visible ? 'visible' : 'hidden'
     }
   })
 }
