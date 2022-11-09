3132369169afba386d80f0f881aee4bd9a93675d
bigdadel <dadeldragon@163.com>
2022-11-09 18:21:07 +0800

    feat: add minimap component

---
+++ packages/xflow/src/components/minimap/index.tsx
@@ -0,0 +1,35 @@
+import React, { FC, useRef, useEffect, useCallback } from 'react'
+import { MiniMap } from '@antv/x6-plugin-minimap'
+import { useGraph } from '../../hooks/graph'
+
+export const Minimap: FC<Omit<MiniMap.Options, 'container'>> = (props) => {
+  const graph = useGraph()
+  const mapRef = useRef<HTMLDivElement | null>(null)
+
+  const initMinimap = useCallback(() => {
+    if (mapRef.current) {
+      const options = { ...props, container: mapRef.current }
+      const minimap = graph.getPlugin<MiniMap>(MiniMap.name)
+
+      if (minimap) {
+        minimap.dispose()
+        // TODO: add setOption function
+        // minimap.setOptions(options)
+
+        minimap.init(graph)
+      } else {
+        const minimap = new MiniMap(options)
+
+        minimap.init(graph)
+
+        graph.use(minimap)
+      }
+    }
+  }, [mapRef, props, graph])
+
+  useEffect(() => {
+    initMinimap()
+  }, [mapRef])
+
+  return <div ref={mapRef} />
+}
