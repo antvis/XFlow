a498a7fc1e2a400f302e6cf2f3d4a4cf7496588a
bigdadel <dadeldragon@163.com>
2022-11-09 10:34:13 +0800

    feat: add format hook

--- packages/xflow/src/hooks/format/index.ts
+++ packages/xflow/src/hooks/format/index.ts
@@ -1,1 +1,41 @@
-export const useFormat = () => {}
+import { useEffect } from 'react'
+import { Export } from '@antv/x6-plugin-export'
+import { Graph } from '@antv/x6'
+import { useGraph } from '../graph'
+
+class XFlowExport {
+  protected export: Export
+  protected options: Export.ToImageOptions
+
+  init(graph: Graph, options: Export.ToImageOptions) {
+    if (!this.export) {
+      this.export = new Export()
+    }
+
+    this.export.init(graph)
+    this.options = options
+  }
+
+  exportPng = (fileName: string) => {
+    this.export.exportPNG(fileName, this.options)
+  }
+
+  exportJPEG = (fileName: string) => {
+    this.export.exportJPEG(fileName, this.options)
+  }
+}
+
+export const SINGLETON = new XFlowExport()
+
+export const useFormat = (options: Export.ToImageOptions) => {
+  const graph = useGraph()
+
+  useEffect(() => {
+    SINGLETON.init(graph, options)
+  }, [graph, options])
+
+  return {
+    exportPng: SINGLETON.exportPng,
+    exportJPEG: SINGLETON.exportJPEG,
+  }
+}
