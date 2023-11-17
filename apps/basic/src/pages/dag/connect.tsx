a1bf3fbb0c5f9b6dcbdf661c055e55f19248e39b
summer <dewjq1479.163.com>
2023-11-17 14:39:58 +0800

    refactor:update dag

---
+++ apps/basic/src/pages/dag/connect.tsx
@@ -0,0 +1,16 @@
+import { useGraphEvent, useGraphStore, } from '@antv/xflow';
+
+
+const Connect = () => {
+  const updateEdge = useGraphStore(state => state.updateEdge)
+  useGraphEvent('edge:connected', ({ edge }) => {
+    updateEdge(edge.id, {
+      animated: false
+    })
+
+  }
+  );
+  return null
+};
+
+export { Connect };
