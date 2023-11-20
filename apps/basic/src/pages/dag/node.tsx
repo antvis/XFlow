851dfaa7aba8af6af0a8254d2218a59accc15a0f
SiYuan.lsy <2228429150@qq.com>
2023-11-20 21:24:19 +0800

    fix: update control dependencies

--- apps/basic/src/pages/dag/node.tsx
+++ apps/basic/src/pages/dag/node.tsx
@@ -6,10 +6,13 @@ import { DAG_EDGE, DAG_NODE } from './shape';
 const InitShape = () => {
   const addNodes = useGraphStore((state) => state.addNodes);
   const addEdges = useGraphStore((state) => state.addEdges);
+  const updateNode = useGraphStore((state) => state.updateNode);
+  const updateEdge = useGraphStore((state) => state.updateEdge);
 
   const initEdge = useCallback(() => {
     addEdges([
       {
+        id: 'initEdge1',
         shape: DAG_EDGE,
         source: {
           cell: 'initNode1',
@@ -19,8 +22,10 @@ const InitShape = () => {
           cell: 'initNode2',
           port: 'initNode2-1',
         },
+        animated: true,
       },
       {
+        id: 'initEdge2',
         shape: DAG_EDGE,
         source: {
           cell: 'initNode2',
@@ -30,8 +35,10 @@ const InitShape = () => {
           cell: 'initNode3',
           port: 'initNode3-1',
         },
+        animated: true,
       },
       {
+        id: 'initEdge3',
         shape: DAG_EDGE,
         source: {
           cell: 'initNode2',
@@ -41,6 +48,7 @@ const InitShape = () => {
           cell: 'initNode4',
           port: 'initNode4-1',
         },
+        animated: true,
       },
     ]);
   }, [addEdges]);
@@ -70,7 +78,7 @@ const InitShape = () => {
         y: 350,
         data: {
           label: '逻辑回归',
-          status: 'success',
+          status: 'running',
         },
         ports: [
           {
@@ -94,7 +102,7 @@ const InitShape = () => {
         y: 500,
         data: {
           label: '模型预测',
-          status: 'failed',
+          status: 'running',
         },
         ports: [
           {
@@ -128,7 +136,35 @@ const InitShape = () => {
         ],
       },
     ]);
-  }, [addNodes]);
+    setTimeout(() => {
+      updateNode('initNode2', {
+        data: {
+          status: 'success',
+        },
+      });
+      updateEdge('initEdge1', {
+        animated: false,
+      });
+    }, 1000);
+    setTimeout(() => {
+      updateNode('initNode4', {
+        data: {
+          status: 'success',
+        },
+      });
+      updateNode('initNode3', {
+        data: {
+          status: 'failed',
+        },
+      });
+      updateEdge('initEdge2', {
+        animated: false,
+      });
+      updateEdge('initEdge3', {
+        animated: false,
+      });
+    }, 2000);
+  }, [addNodes, updateNode, updateEdge]);
 
   useEffect(() => {
     addNodeInit();
