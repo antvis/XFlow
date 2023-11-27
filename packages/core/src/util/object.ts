56901ff0c1ef73fddc3c27bbe8da9c9d7290a417
妙子 <759870535@qq.com>
2023-11-27 11:36:52 +0800

    fix: core  Cannot read properties of null (reading hasOwnProperty)

--- packages/core/src/util/object.ts
+++ packages/core/src/util/object.ts
@@ -49,7 +49,7 @@ export function apply(target: any, patchItem: any): any {
     if (!targetResult.hasOwnProperty(k)) targetResult[k] = patchItem[k]; //This ensure the key exists and TS can't throw a wobbly over an undefined key
     if (patchItem[k] === null) {
       delete targetResult[k];
-    } else {
+    } else if (targetResult[k] !== null) {
       targetResult[k] = apply(targetResult[k], patchItem[k]);
     }
   });
